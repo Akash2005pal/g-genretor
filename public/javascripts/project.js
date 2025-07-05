48ea167aa85e4fc58708e3e31bea63a42576bd05
Akash2005pal <akashbaghel73030@gmail.com>
2025-07-05 17:32:41 +0530

    update

--- public/javascripts/project.js
+++ public/javascripts/project.js
@@ -1,10 +1,29 @@
-const { json } = require("express")
 
-$(document).ready(function(){
-    $.get('/employee/fetch_all_states',function(response){
-        //alert(json.stringify(response.data))
-        response.data.map((item)=>{
-            $('#stateid').append($('<option>').text(item.statename).val(item.stateid))
+
+$(document).ready(function () {
+    $.getJSON('/employee/fetch_all_states', function (response) {
+        // alert(JSON.stringify(response.data))
+        response.data.map((item) => {
+            $('#stateid').append($('<option>').text(item.statename).val(item.id))
+        });
+    });
+
+    $('#stateid').change(function () {
+        $.getJSON('/employee/fetch_all_city', { stateid: $('#stateid').val() }, function (response) {
+           // alert(JSON.stringify(response.data))
+           $('#cityid').empty()
+           $('#cityid').append($('<option>').text('Select City'))
+            response.data.map((item) => {
+                $('#cityid').append($('<option>').text(item.cityname).val(item.cityid))
+            });
+            
+        });
+       
+    });
+     $('#picture').change(function (event){
+            var file=URL.createObjectURL(event.target.files[0] )
+            alert(file)    
+             $('#epic').attr('src',file) 
+             
         })
-    })
-})
+});
--- routes/employee.js
+++ routes/employee.js
@@ -5,17 +5,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/employee_interface', function (req, res, next) {
-  res.render('employeeform');
+  res.render('employeeform', { title: 'Express' });
 });
 
 router.get('/fetch_all_states', function (req, res) {
   pool.query('select * from state', function (error, result) {
-    if (error)
-    {console.log(error)
+    if (error) {
+      console.log(error)
       res.json({ status: false, data: [], message: 'Error in Query Pls Contact Data Administrator...' })
     }
-   else
-    {
+    else {
+      res.json({ status: true, data: result, message: 'Successful' })
+    }
+
+  })
+
+})
+router.get('/fetch_all_city', function (req, res) {
+  pool.query('select * from city where state_id=?', [req.query.stateid], function (error, result) {
+    if (error) {
+      console.log(error)
+      res.json({ status: false, data: [], message: 'Error in Query Pls Contact Data Administrator...' })
+    }
+    else {
       res.json({ status: true, data: result, message: 'Successful' })
     }
 
